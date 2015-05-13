8ccd71efc841a7384fd3caaeee7c3e1a0597a5de
Kevin Delannoy <delannoyk@gmail.com>
2015-05-13 18:56:07 +0200

    Initial commit

---
+++ YAJSONObject.podspec
@@ -0,0 +1,21 @@
+Pod::Spec.new do |s|
+  s.name         = 'YAJSONObject'
+  s.version      = '1.0.0'
+  s.license      =  { :type => 'MIT' }
+  s.homepage     = 'https://github.com/delannoyk/YAJSONObject'
+  s.authors      = {
+    'Kevin Delannoy' => 'delannoyk@gmail.com'
+  }
+  s.summary      = 'Yet Another JSON Object.'
+
+# Source Info
+  s.platform     =  :ios, '8.0'
+  s.source       =  {
+    :git => 'https://github.com/delannoyk/YAJSONObject.git',
+    :tag => s.version.to_s
+  }
+  s.source_files = 'YAJSONObject.swift'
+  s.framework    =  'UIKit'
+
+  s.requires_arc = true
+end
---
+++ YAJSONObject.swift
@@ -0,0 +1,99 @@
+//
+//  YAJSONObject.swift
+//  YAJSONObject
+//
+//  Created by Kevin DELANNOY on 13/05/15.
+//  Copyright (c) 2015 Kevin Delannoy. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - YAJSONObject
+////////////////////////////////////////////////////////////////////////////
+
+public class YAJSONObject: SequenceType {
+    internal let value: AnyObject?
+    internal var index: Int = 0
+
+    public init(_ value: AnyObject?) {
+        self.value = value
+    }
+
+    public subscript(index: Int) -> YAJSONObject {
+        return (value as? [AnyObject]).map { YAJSONObject($0[index]) } ?? YAJSONObject(nil)
+    }
+
+    public subscript(key: String) -> YAJSONObject {
+        return (value as? NSDictionary).map { YAJSONObject($0[key]) } ?? YAJSONObject(nil)
+    }
+
+    public func generate() -> GeneratorOf<YAJSONObject> {
+        return GeneratorOf<YAJSONObject> {
+            if self.index + 1 < 0 {
+                return nil
+            }
+            return self[self.index++]
+        }
+    }
+
+    public func map<U>(f: YAJSONObject -> U) -> [U]? {
+        if let value = value as? [AnyObject] {
+            return value.map({ f(YAJSONObject($0)) })
+        }
+        return nil
+    }
+}
+
+public extension YAJSONObject {
+    public var anyObjectValue: AnyObject? {
+        return value
+    }
+
+    public var intValue: Int? {
+        return (value as? Int)
+    }
+
+    public var uint64Value: UInt64? {
+        return (value as? UInt64)
+    }
+
+    public var doubleValue: Double? {
+        return (value as? Double)
+    }
+
+    public var boolValue: Bool? {
+        return (value as? Bool)
+    }
+
+    public var stringValue: String? {
+        return (value as? String)
+    }
+
+    public var URLValue: NSURL? {
+        return (value as? String).map { NSURL(string: $0) } ?? nil
+    }
+
+    public func dateValue(dateFormat: String) -> NSDate? {
+        let date: NSDate?? = stringValue.map {
+            let dateFormatter = NSDateFormatter()
+            dateFormatter.dateFormat = dateFormat
+            return dateFormatter.dateFromString($0)
+        }
+        return date ?? nil
+    }
+
+    public func arrayValue<T>(mapping: YAJSONObject -> T?) -> [T]? {
+        if let JSONArray =  value as? [AnyObject] {
+            let values = JSONArray.map { mapping(YAJSONObject($0)) }
+            return values.reduce([], combine: { (l, o) -> [T] in
+                if let o = o {
+                    return l + [o]
+                }
+                return l
+            })
+        }
+        return nil
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////
