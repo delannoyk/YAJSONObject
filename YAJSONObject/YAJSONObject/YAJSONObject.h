e1a64adc7edffbb3813ab0e277c70255f5a9ef64
Kevin Delannoy <delannoyk@gmail.com>
2015-09-23 18:50:27 +0200

    Added project container for carthage

---
+++ YAJSONObject/YAJSONObject/YAJSONObject.h
@@ -0,0 +1,19 @@
+//
+//  YAJSONObject.h
+//  YAJSONObject
+//
+//  Created by Kevin DELANNOY on 23/09/15.
+//  Copyright © 2015 Kevin Delannoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YAJSONObject.
+FOUNDATION_EXPORT double YAJSONObjectVersionNumber;
+
+//! Project version string for YAJSONObject.
+FOUNDATION_EXPORT const unsigned char YAJSONObjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YAJSONObject/PublicHeader.h>
+
+
